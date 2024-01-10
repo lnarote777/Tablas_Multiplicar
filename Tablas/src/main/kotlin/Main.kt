5cee07c2f17ac4b271a1b3d71960dd9d53439be0
lnarote777 <nolara@iesrafaelalberti.es>
2024-01-10 19:52:44 +0100

    Initial commit

--- Tablas/src/main/kotlin/Main.kt
+++ Tablas/src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * Solicita un número entero en un rango.
  *
@@ -11,14 +14,12 @@ fun pedirNum(min: Int, max: Int): Int {
     print("Introduzca un número para generar su tabla de multiplicación: ")
     var num = readln().toInt()
 
-    if (num < 1){
+    while (num < min || num > max){
         print("Número no válido.")
         print("Introduzca un número para generar su tabla de multiplicación: ")
         num = readln().toInt()
-
-    }else if (num > 100){
-        print("Número no válido")
     }
+
     return num
 }
 
@@ -31,6 +32,10 @@ fun pedirNum(min: Int, max: Int): Int {
  */
 fun pregunta(text: String){
 
+    print(text)
+    var respuesta = readln()
+
+
 
 
 }
@@ -39,14 +44,14 @@ fun main() {
     //TODO: Solicitar la introducción de un número entre 1 y 100 y mostrar su table de multiplicar...
     //Hasta que se responda negativamente a la pregunta "¿Desea generar otra tabla de multiplicación? (s/n)"
 
-    val num = 2
+    val num = pedirNum(min = 1, max = 100)
+
     val tablas = Array<String>(11){i -> "$i x $num = ${num * i}"}
     for (linea in tablas){
         println(linea)
     }
 
-    val min = 1
-    val max = 100
-    pedirNum(min, max)
+    val respuesta = pregunta(text = "¿Desea generar otra tabla de multiplicación? (s/n)" )
+
 
 }
